96a325b51fd4e28f0b9566fa1f7aa8be0a7ea11e
Jessiah <s3731166@student.rmit.edu.au>
2020-05-26 20:24:11 +1000

    Pushint Post Demo

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
     gravtastic
   has_secure_password
   validates :phone, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, numericality: true
   validates :email, presence: true, uniqueness: true
+  
  
-  has_many :users, dependent: :destroy
+  has_many :posts, dependent: :destroy
 
 end
