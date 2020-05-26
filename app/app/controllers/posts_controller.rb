0f3f295458320cd5b0a2dfe96365caa74e303f71
Jessiah <s3731166@student.rmit.edu.au>
2020-05-26 16:39:07 +1000

    User Has Post

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   helper_method :current_user
+
   before_action :authenticate_user!, except: [:show]
+  # skip_before_action :authenticate_user!, only: [:show]
+
 
 
   # GET /posts
