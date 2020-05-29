3d89e73b65e0c8284e8a4ae7494f93684b39f518
Jessiah <s3731166@student.rmit.edu.au>
2020-05-29 15:15:16 +1000

    Branching for our credit requirements

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, except: [:show]
   # skip_before_action :authenticate_user!, only: [:show]
-  respond_to :html, :js
+  # respond_to :html, :js
   # GET /posts
   # GET /posts.json
   def index
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_062751) do
+ActiveRecord::Schema.define(version: 2020_05_29_032116) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
